f9c6fedc9df3eedd77efebe5162233bfaae8cf9e
51241636 <pisepala@gmail.com>
2026-05-15 19:42:43 +0530

    comment add login model

--- model/loginModel.js
+++ model/loginModel.js
@@ -1,11 +1,12 @@
-
+// save user name and assword within local storage
 localStorage.setItem("userName","pisepala@gmail.com");
 localStorage.setItem("password","1234");
 
-
+// get saved usename
 let getUserName= ()=>{
    return  localStorage.getItem("userName");
 }
+// get saved password
 let getUserPassword= ()=>{
    return  localStorage.getItem("password");
 }
